b3700a9d357633db8ec5de3c43d28fc1d52ccb07
Julio Cesar Ramos de Oliveira <juliocesarramos766@gmail.com>
2025-01-07 21:37:27 -0300

    07/01/2025 - noite

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-//Outbind decorator
-function autobind(target, methodname, descriptor) {
+function autobind(_, _2, descriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor = {
         configurable: true,
@@ -17,7 +16,6 @@ function autobind(target, methodname, descriptor) {
     };
     return adjDescriptor;
 }
-//Projectinputclasss
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -31,9 +29,26 @@ class ProjectInput {
         this.configure();
         this.attach();
     }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = parseInt(this.peopleInputElement.value);
+        if (enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredTitle.trim().length === 0) {
+            alert('Invalid input, please try again.');
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
     submitHandler(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
     }
     configure() {
         this.element.addEventListener('submit', this.submitHandler);
@@ -46,3 +61,4 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjinput = new ProjectInput();
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 //Outbind decorator
-function autobind(target: any, methodname: string, descriptor: PropertyDescriptor){
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
         configurable: true,
@@ -36,10 +36,37 @@ class ProjectInput{
         this.configure()
         this.attach()
         }
+
+        private gatherUserInput(): [string, string, number] | void{
+            const enteredTitle = this.titleInputElement.value
+            const enteredDescription = this.descriptionInputElement.value
+            const enteredPeople = parseInt(this.peopleInputElement.value)
+
+            if(enteredTitle.trim().length === 0 || 
+            enteredDescription.trim().length === 0 || 
+            enteredTitle.trim().length === 0){
+                alert('Invalid input, please try again.')
+            }else{
+                return [enteredTitle, enteredDescription, +enteredPeople]
+         
+            }
+        }
+
+        private clearInputs(){
+            this.titleInputElement.value = ''
+            this.descriptionInputElement.value = ''
+            this.peopleInputElement.value = ''
+        }
+
         @autobind
         private submitHandler(event: Event){
             event.preventDefault();
-            console.log(this.titleInputElement.value)
+            const userInput = this.gatherUserInput();
+            if(Array.isArray(userInput)){
+                const[title, desc, people] = userInput
+                console.log(title, desc, people);
+                this.clearInputs()
+            }
         }
 
         private configure(){
